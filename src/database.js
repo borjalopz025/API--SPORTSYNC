162c3dba0fd6fb7a7fbf8c5fdc48f7f13aeb399a
borja <borjaswim1995@gmail.com>
2023-11-11 21:23:28 +0100

    preparacion para vercel

--- src/database.js
+++ src/database.js
@@ -1,15 +1,12 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host               : "sportsync.cpirdo7txrb3.eu-west-3.rds.amazonaws.com",
-    user               : "admin",
-    password           : "Sportsync1234",
-    database           : "SportSync",
-    waitForConnections : true,
-    connectionLimit    : 10,
-    maxIdle            : 10,
-    idleTimeout        : 60000,
-    queueLimit         : 0
+    host:      process.env.DB_HOST        || "sportsync.cpirdo7txrb3.eu-west-3.rds.amazonaws.com",
+    user:      process.env.DB_USER        || "admin",
+    password:  process.env.DB_PASSWORD    || "Sportsync1234",
+    database:  process.env.DB_DATABASE    || "SportSync",
+    port:      process.env.DB_PORT        || 3306
+    
 }).promise();
 
 console.log("Conexión con la BBDD creada");
