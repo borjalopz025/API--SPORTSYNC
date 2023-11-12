1bb10d91cc69f3ad5bc2bcd200b36b687bd1f83d
borja <borjaswim1995@gmail.com>
2023-11-12 06:01:32 +0100

    cuarta vez :c

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -5,6 +5,12 @@ const getUser =  async(req, res) =>
 {
   try 
   {
+
+         
+      console.log(sql);
+      let [result] = await pool.query(sql)
+      console.log(result); 
+      
       res.send("api desplegada")
       
   } 
