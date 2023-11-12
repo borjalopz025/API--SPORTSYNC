6a6be83519505616401f9b1f884eb78cdc3c5fa5
borja <borjaswim1995@gmail.com>
2023-11-12 06:25:11 +0100

    acabado

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,24 +1,8 @@
 const { pool } = require('../database');
 
 
-const getUser =  async(req, res) =>
-{
-  try 
-  {
 
-         
-      console.log(sql);
-      let [result] = await pool.query(sql)
-      console.log(result); 
-      
-      res.send("api desplegada")
-      
-  } 
-  catch (error) 
-  {
-      console.log(error);
-  }
  
-}
 
-module.exports= {getUser}
+
+module.exports= {}
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -2,7 +2,7 @@ const {Router} = require ("express")
 const router = Router();
 const userCtrl = require("../controller/user.controller");
 
-router.get("/user", userCtrl.getUser);
+
 
 
 
