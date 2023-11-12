87b1a358f88d084124b4c0aa495742cbf7a44c1a
borja <borjaswim1995@gmail.com>
2023-11-12 06:10:40 +0100

    quita vez

---
+++ src/routers/user.routers.js
@@ -0,0 +1,9 @@
+const {Router} = require ("express")
+const router = Router();
+const userCtrl = require("../controller/user.controller");
+
+router.get("/user", userCtrl.getUser);
+
+
+
+module.exports = router;
